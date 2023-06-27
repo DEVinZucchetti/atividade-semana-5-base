46fab1d7ce48920b389eff8a755ca98fdfee5a64
romeuic <casarottori@gmail.com>
2023-06-27 00:55:38 -0300

    adiciona arquivos basicos da atividade m1-s05

---
+++ exercicio1/app.js
@@ -0,0 +1,7 @@
+console.log("m1-s05-e1");
+
+const idade = prompt("Informe sua idade:");
+
+// realize as comparações necessárias e exiba com alert
+
+alert("Conclúa o desenvolvimento do exercício.");
