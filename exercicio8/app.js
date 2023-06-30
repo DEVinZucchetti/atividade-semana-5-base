83d9f93cd68d964bb4523217f51e38d5f8b4f0c6
romeuic <casarottori@gmail.com>
2023-06-29 21:58:31 -0300

    adiciona solucao exercicio 8

---
+++ exercicio8/app.js
@@ -0,0 +1,9 @@
+console.log("m1-s05-e8");
+
+const valorInicial = 77;
+const raiz = 4;
+
+// afazer: realizar as comparações necessárias e exibir no alert
+
+// exemplo de concatenação de strings com string template
+console.log(`Resolução Ex. 8: vi = ${valorInicial} r = ${raiz}`);
